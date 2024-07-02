8a17ffd376728af72ddffe13ff0df0ab04d74e1a
Corina Reitemeyer <corina@corina.org>
2024-07-02 20:27:11 +1200

    Create component files

---
+++ client/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <div>
+          <p>&copy; 2024 Corina Reitemeyer. All rights reserved.</p>
+        </div>
+      </footer>
+    </>
+  )
+}

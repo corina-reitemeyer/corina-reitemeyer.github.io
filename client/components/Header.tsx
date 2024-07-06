306d6ddfc20300e6202f0a599769e0b8cc8eb06e
Corina Reitemeyer <corina@corina.org>
2024-07-06 21:19:08 +1200

    Add links to Header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,3 +1,21 @@
+import { Link } from 'react-router-dom'
+
 export default function Header() {
-  return()
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
 }

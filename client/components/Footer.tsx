70a47a27e630107ae12a25b823210b5f3a789d26
Corina Reitemeyer <corina@corina.org>
2024-07-07 19:53:38 +1200

    Style Footer.tsx

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
     <>
-      <footer>
-        <div>
-          <p>&copy; 2024 Corina Reitemeyer. All rights reserved.</p>
+      <footer className="w-full border-t border-gray-200 bg-white py-4">
+        <div className="container mx-auto pt-4 text-center">
+          <p className="mb-4 mt-4">&copy; 2024 Corina Reitemeyer.</p>
         </div>
       </footer>
     </>

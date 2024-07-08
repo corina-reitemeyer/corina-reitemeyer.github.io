68a1bb42d2b92fcc55ffd4aec2952100f77b1dc3
Corina Reitemeyer <corina@corina.org>
2024-07-08 13:29:13 +1200

    Add useState and evenhandler to form

--- client/pages/Contact.tsx
+++ client/pages/Contact.tsx
@@ -1,19 +1,62 @@
+import { useState } from 'react'
+
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormData({
+      name: '',
+      email: '',
+      message: '',
+    })
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Name:
-          <input type="text" name="value" />
+          <input
+            type="text"
+            name="value"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </label>
         <br />
         <label>
           Email:
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
         </label>
         <label>
           Message:
-          <input name="email" />
+          <input
+            name="email"
+            value={formData.message}
+            onChange={handleChange}
+            rows={4}
+            required
+          />
         </label>
         <br />
         <button type="submit">Submit</button>

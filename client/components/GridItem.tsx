2f8cf473cdd50d135dccdee26603132e03a7e42c
Corina Reitemeyer <corina@corina.org>
2024-07-08 16:01:38 +1200

    Add hover effect on each grid item

--- client/components/GridItem.tsx
+++ client/components/GridItem.tsx
@@ -1,15 +1,28 @@
+import { Link } from 'react-router-dom'
+
 interface GridItemProps {
   image: string
   alt?: string
+  slug: string // Unique identifier for the case study
+  title: string
 }
 
-const GridItem: React.FC<GridItemProps> = ({ image, alt }) => {
+const GridItem: React.FC<GridItemProps> = ({ image, alt, slug, title }) => {
+  const url = `/projects/${slug}`
+
   return (
-    <>
-      <div className="aspect-w-16 aspect-h-9 flex items-center justify-center">
-        <img src={image} alt={alt || 'Portfolio Item'} />
+    <Link to={url} className="group relative block">
+      <div className="aspect-w-16 aspect-h-9 relative overflow-hidden">
+        <img
+          src={image}
+          alt={alt || 'Portfolio Item'}
+          className="scale-100 transform object-cover transition-transform duration-300 group-hover:scale-110"
+        />
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+          <span className="text-lg font-bold text-white">{title}</span>
+        </div>
       </div>
-    </>
+    </Link>
   )
 }
 
--- client/pages/Projects.tsx
+++ client/pages/Projects.tsx
@@ -5,22 +5,34 @@ interface ProjectsProps {
 }
 
 const Projects: React.FC<ProjectsProps> = () => {
-  const mockImages = [
-    'https://via.placeholder.com/1300x750',
-    'https://via.placeholder.com/1300x750',
-    'https://via.placeholder.com/1300x750',
-    'https://via.placeholder.com/1300x750',
-    'https://via.placeholder.com/1300x750',
-    'https://via.placeholder.com/1300x750',
-  ]
+  // const mockImages = [
+  //   'https://via.placeholder.com/1300x750',
+  //   'https://via.placeholder.com/1300x750',
+  //   'https://via.placeholder.com/1300x750',
+  //   'https://via.placeholder.com/1300x750',
+  //   'https://via.placeholder.com/1300x750',
+  //   'https://via.placeholder.com/1300x750',
+  // ]
 
+  const mockImages = [
+    {
+      src: 'https://via.placeholder.com/1300x750',
+      alt: 'Description of image 1',
+      slug: 'case-study-1',
+    },
+    {
+      src: 'https://via.placeholder.com/1300x750',
+      alt: 'Description of image 2',
+      slug: 'case-study-2',
+    },
+  ]
   return (
     <>
       <div className="mx-auto px-8 py-10">
         <div className="grid grid-cols-1 justify-items-center gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-3">
           {mockImages.map((image, index) => (
             <div key={index} className="flex items-center justify-center">
-              <GridItem image={image} />
+              <GridItem image={image.src} alt={image.alt} url={image.url} />
             </div>
           ))}
         </div>

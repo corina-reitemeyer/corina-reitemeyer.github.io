2c9572b9e715033fe828de60a7369945bc81e99a
Corina Reitemeyer <corina@corina.org>
2024-07-10 20:49:22 +1200

    useEffect function to fetch  json file to CaseStudy.tsx

--- client/pages/CaseStudy.tsx
+++ client/pages/CaseStudy.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+
 const CaseStudy = () => {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    fetch('/caseStudyData.json')
+      .then((response) => response.json)
+      .then((data) => setProjects(data.projects))
+      .catch((error) => console.error('Error fetching data:', error))
+  }, [])
   return (
     <>
       <div className="flex min-h-screen items-center justify-center">

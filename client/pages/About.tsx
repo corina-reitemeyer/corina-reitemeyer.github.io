9e2be9ee1dd69f802bb4977d0e3d82339cc5da64
Corina Reitemeyer <corina@corina.org>
2024-11-04 14:56:17 +1300

    Update About page

--- client/pages/About.tsx
+++ client/pages/About.tsx
@@ -10,23 +10,34 @@ const About = () => {
           />
           <h2 className="py-8 text-3xl font-bold">Hi! I&apos;m Corina 👋</h2>
           <p className="paragraph">
-            I&apos;m a Full Stack Developer & Senior UX/UI Designer, backed with
-            6-7 years experience working in the tech industry. My journey into
-            technology began in my teens, where I explored both design and
-            coding out of personal interest. After dedicating a significant
-            portion of my career to UX/UI design, I decided to shift gears from
-            design to delve deeper into programming. In March 2024, I enrolled
-            in a fast-paced 17-week bootcamp at Dev Academy Aotearoa to
-            specialise in Full Stack Web Development.
+            I&apos;m a Senior UX/UI designer and Full Stack Developer with a
+            passion for data analytics, allowing me to create user-centered
+            solutions that are both functional and impactful. With 6-7 years of
+            experience in tech under my belt, I love uncovering the stories that
+            data tells, and crafting experiences that truly resonate with
+            people.
           </p>
           <p className="paragraph">
-            My aim is to create easy-to-use software that enhances users&apos;
-            trust and improves their experience with digital products, keeping
-            things simple and enjoyable, whilst building software that is robust
-            and easy to maintain. I prioritise best practices and thrive on
-            continuous learning, embracing new technologies to deliver valuable
-            results. I find fulfilment in tackling challenges, whether it&apos;s
-            through coding or design.
+            My tech journey kicked off in my teens, sparked by a curiosity for
+            both design and coding. After diving deep into UX/UI design for
+            several years, I decided to pivot toward programming. In March 2024,
+            I jumped into a fast-paced 17-week bootcamp at Dev Academy to
+            specialise in Full Stack Web Development. Not long after, I plunged
+            into data analytics to see how I could blend my UX background with
+            my technical skills to uncover valuable insights and create
+            meaningful solutions.
+          </p>
+          <p className="paragraph">
+            I&apos;m all about best practices and thrive on continuous learning,
+            always eager to embrace new technologies to deliver awesome results.
+            I find joy in tackling challenges, whether it&apos;s through coding
+            or design, and finding interesting insights and patterns in
+            datasets.
+          </p>
+          <p className="paragraph">
+            In 2019, I co-founded HackathonsNZ, where I helped capture event
+            moments and boost the social media presence of Hackathon events in
+            Aotearoa.
           </p>
           <p className="paragraph">
             When I&apos;m not in front of my computer, I enjoy Latin dancing and

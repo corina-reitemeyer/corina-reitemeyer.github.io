e1e86d3fe84f0b889ebbb59c49d2e72cd8c963f0
Corina Reitemeyer <corina@corina.org>
2025-01-09 16:23:26 +1300

    Remove grow-grub and add Hero Section

--- client/pages/Projects.tsx
+++ client/pages/Projects.tsx
@@ -11,6 +11,19 @@ const Projects: React.FC<ProjectsProps> = () => {
   const projects = caseStudyData as Project[]
   return (
     <>
+      <header className="bg-white py-20 text-center">
+        <h1 className="text-3xl font-bold text-gray-900 md:text-4xl">
+          Hey! I’m Corina 👋
+        </h1>
+        <h2 className="mt-2 text-lg text-gray-700 md:text-xl">
+          UX/UI Designer geeking over data and design systems.
+        </h2>
+        <p className="mx-auto mt-4 max-w-xl text-base leading-relaxed text-gray-600">
+          I’m passionate about creating user-centered designs for web and mobile
+          apps, blending creativity with usability to craft delightful user
+          experiences.
+        </p>
+      </header>
       <div className="flex justify-center px-14 py-8">
         <div className="2xl:max-w-8xl grid w-full max-w-full grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-6 lg:max-w-7xl xl:max-w-fit">
           {projects.map((project) => (

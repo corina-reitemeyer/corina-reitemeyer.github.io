2cc5eb532ffe5057aa87f1a9103d5bd1dcc6c13a
Corina Reitemeyer <corina@corina.org>
2025-01-10 17:27:03 +1300

    Update footer and contact page

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className="w-full border-t border-gray-200 bg-white py-4">
+      <footer className="w-full bg-white py-4">
         <div className="container mx-auto pt-4 text-center">
           <p className="mb-4 mt-4">&copy; 2024 Corina Reitemeyer.</p>
         </div>
--- client/pages/Contact.tsx
+++ client/pages/Contact.tsx
@@ -1,36 +1,37 @@
 const Contact = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center py-48">
-        <div className="w-full max-w-3xl px-4">
-          <div className="mb-6 flex w-full items-center justify-center">
-            <img src="/images/sunflower.png" alt="" className="max-w-xs" />
-          </div>
-          <h2 className="py-4 text-3xl font-bold">Get in touch</h2>
-          <p className="paragraph">
-            Have an idea you want to talk about? Let&apos;s have a chat!
-          </p>
+      <div className="flex flex-col items-center justify-center bg-gray-50 py-32">
+        <div className="w-full max-w-4xl rounded-3xl border-4 border-black bg-white p-10 px-6 text-center">
+          {/* Heading */}
+          <h2 className="font-heading mb-6 text-3xl font-extrabold text-black">
+            Get in Touch
+          </h2>
 
-          <p>
-            Send me an{' '}
-            <a
-              href="mailto:corina@corina.org"
-              className="text-amber-600 hover:underline"
-            >
-              email
-            </a>{' '}
-            or connect with me on{' '}
-            <a
-              href="https://www.linkedin.com/in/corinareitemeyer"
-              target="_blank"
-              className="text-amber-600 hover:underline"
-              rel="noreferrer"
-            >
-              LinkedIn
-            </a>
-            . Whether it&apos;s about a project, a job opportunity, or would
-            like some design mentoring (NZ only) - I&apos;m here to help 😊
-          </p>
+          {/* Text Wrapper with Max Width */}
+          <div className="mx-auto max-w-xl">
+            {/* Contact Info */}
+            <p className="font-body text-lg leading-relaxed text-gray-800">
+              Send me an{' '}
+              <a
+                href="mailto: corina@corina.org"
+                className="text-black underline decoration-4 hover:text-purple-500"
+              >
+                email
+              </a>{' '}
+              or connect with me on{' '}
+              <a
+                href="https://www.linkedin.com/in/corinareitemeyer/"
+                target="_blank"
+                className="text-black underline decoration-4 hover:text-purple-500"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>
+              . Whether it&apos;s about a project, a job opportunity, or you’d
+              like some design mentoring — I&apos;m here to help 😊
+            </p>
+          </div>
         </div>
       </div>
     </>
